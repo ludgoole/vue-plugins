030002b57ff6327a94c699230ddded8bbf1c30f9
Ludgoole <ludgoole@163.com>
2020-11-29 17:08:26 +0800

    ✨feat(components/plugins): 新增原生滚动操作

---
+++ src/components/plugins/TheScroll.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="the-scroll flex flex-center">
+    <ul ref="list">
+      <li v-for="n in 10" :key="n" @click="scrollTo">{{n}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      container: null,
+      list: []
+    }
+  },
+  methods: {
+    scrollTo (e) {
+      const container = this.container
+      const el = e.target
+
+      if (container.scrollTop < container.scrollHeight - container.offsetHeight) {
+        container.scrollTo(0, el.offsetTop - container.offsetTop)
+      } else {
+        console.log('我是有底线的~~')
+      }
+    },
+    onScroll () {
+      const container = this.container
+      if (container.scrollTop <= 0) {
+        console.log('别拉了，到顶了--')
+      } else if (container.scrollTop >= container.scrollHeight - container.offsetHeight) {
+        console.log('别拉了，到底了--')
+      } else {
+        console.log('我要开始滚啦--')
+      }
+    }
+  },
+  mounted () {
+    this.container = this.$refs.list
+    this.container.addEventListener('scroll', this.onScroll)
+
+    this.$once('hook:beforeDestory', () => {
+      this.container.removeEventListener('scroll', this.onScroll)
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.the-scroll {
+  ul {
+    width: 200px;
+    height: 200px;
+    overflow-y: scroll;
+    li {
+      line-height: 30px;
+      &:nth-child(2n + 1) {
+        background: lightblue;
+      }
+      &:nth-child(2n) {
+        background: lightcoral;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheScroll></TheScroll>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TheScroll from '@/components/plugins/TheScroll.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TheScroll
   }
 }
 </script>
