ec00e211fc415cc97d1a6167e8232ad34b6d89c5
hengxiao <hengxiao@iflytek.com>
2021-07-10 12:00:53 +0800

    ✨ feat(mock): 新增mock数据

---
+++ src/mock/bagua.js
@@ -0,0 +1,10 @@
+export default [
+  { order: 1, name: '乾', extName: '天', guaXiang: [1, 1, 1] },
+  { order: 2, name: '兑', extName: '泽', guaXiang: [0, 1, 1] },
+  { order: 3, name: '离', extName: '火', guaXiang: [1, 0, 1] },
+  { order: 4, name: '震', extName: '雷', guaXiang: [0, 0, 1] },
+  { order: 5, name: '巽', extName: '风', guaXiang: [1, 1, 0] },
+  { order: 6, name: '坎', extName: '水', guaXiang: [0, 1, 0] },
+  { order: 7, name: '艮', extName: '山', guaXiang: [1, 0, 0] },
+  { order: 8, name: '坤', extName: '地', guaXiang: [0, 0, 0] }
+]
