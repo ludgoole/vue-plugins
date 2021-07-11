3d1649779751e1d3cf89cc70ca55c84c7a3c3fff
hengxiao <hengxiao@iflytek.com>
2021-07-11 20:53:14 +0800

    🐳 chore(stylelintrc): 修复格式问题

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
     'function-url-quotes': 'always',
-    indentation: 4, //缩进4位
+    indentation: 4, // 缩进4位
     'number-leading-zero': null,
     'unit-whitelist': ['em', 'rem', 's', '%', 'px', 'deg'],
     'unit-case': 'lower'
