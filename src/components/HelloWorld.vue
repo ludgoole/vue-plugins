687d5fd56d7d63b47281389c413839cb12554988
hengxiao <hengxiao@iflytek.com>
2021-08-08 21:40:07 +0800

    🐳 chore(HelloWorld): 更改主题色

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -62,7 +62,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
 }
@@ -78,7 +78,7 @@ li {
 }
 
 a {
-  color: #42b983;
+  color: $color-primary;
 }
 
 .msg {
